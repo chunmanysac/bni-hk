247b51e156fd3c19666c9c1508a578b0ec3b2835
chunmanysac <109230495+chunmanysac@users.noreply.github.com>
2026-03-21 10:24:12 +0800

    Rewrite sac-products.js: inject into DB.offers with 7折/8折 discounts

--- sac-products.js
+++ sac-products.js
@@ -1,147 +1,123 @@
-/* SAC Global Products - Auto-seeded from Shopline store
+/* SAC Global Products - Injected into DB.offers
  * Disney Britto & PlayShifu: 7折 (30% off)
  * All other brands: 8折 (20% off)
- * Source: https://sacgloballtd.shoplineapp.com/zh-hant/
  */
-(function seedSACProducts() {
-  const sacProducts = [
-    // === PAGE 1: Disney Britto + Robosen ===
-    {name:"DSBRT Minnie Mouse 3.25 Mini",price:229,brand:"Disney Britto",cat:"Figurine",img:"https://sacgloballtd.shoplineapp.com/zh-hant/products/dsbrt-minnie-mouse-mini-figurine"},
-    {name:"Disney Britto \u67f4\u90e1\u8c93\u8ff7\u4f60\u516c\u4ed4",price:229,brand:"Disney Britto",cat:"Figurine",img:"https://sacgloballtd.shoplineapp.com/zh-hant/products/dsbrt-cheshire-cat-mini-figurine"},
-    {name:"Disney Britto Midas Mickey Mouse Figurine",price:779,brand:"Disney Britto",cat:"Figurine",img:"https://sacgloballtd.shoplineapp.com/zh-hant/products/dsbrt-midas-mickey-mouse-figurine"},
-    {name:"Disney Britto Eeyore Mini Figurine",price:229,brand:"Disney Britto",cat:"Figurine",img:"https://sacgloballtd.shoplineapp.com/zh-hant/products/dsbrt-eeyore-mini-figurine"},
-    {name:"Disney Britto Mickey & Minnie Love Figurine",price:779,brand:"Disney Britto",cat:"Figurine",img:"https://sacgloballtd.shoplineapp.com/zh-hant/products/dsbrt-mickey-minnie-love-fig"},
-    {name:"Robosen Flagship Soundwave G1 Robot (EN)",price:7599,brand:"Robosen",cat:"Robot",img:""},
-    {name:"Disney Britto Simba & Nala Figurine",price:1053,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"DSBRT Mickey & Minnie Wedding",price:1169,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto Jasmine Figurine",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto Bambi & Mother Figurine",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto Tigger Figurine",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto Tinker Bell Figurine",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto Sorcerer Mickey Fantasia Figurine",price:1053,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Stitch Nurses Costume",price:249,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Robosen\u5927\u9ec3\u8702\u6a5f\u68b0\u4ebaG1\u65d7\u8266\u7248 (\u4e2d\u6587)",price:9093,brand:"Robosen",cat:"Robot",img:""},
-    {name:"Robosen\u5927\u9ec3\u8702\u6a5f\u68b0\u4ebaG1\u65d7\u8266\u7248 (\u82f1\u6587)",price:9399,brand:"Robosen",cat:"Robot",img:""},
-    {name:"Disney Britto \u53f2\u8fea\u4ed4\u8ff7\u4f60\u516c\u4ed4",price:229,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Donald Duck 90th",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - 8\" Steamboat Willie",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Stitch Big Fig",price:1799,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Robosen \u9ea5\u52a0\u767bG1\u65d7\u8266\u6a5f\u68b0\u4eba (\u4e2d\u6587)",price:10940,brand:"Robosen",cat:"Robot",img:""},
-    {name:"Robosen \u67ef\u67cf\u6587\u7cbe\u82f1\u7248\u6a5f\u68b0\u4eba(\u4e2d\u6587)",price:5699,brand:"Robosen",cat:"Robot",img:""},
-    {name:"\u5df4\u65af\u5149\u5e74\u73a9\u5177\u7e3d\u52d5\u54e1\u7248 (\u4e2d\u6587)",price:4399,brand:"Robosen",cat:"Robot",img:""},
-    {name:"\u5df4\u65af\u5149\u5e74\u73a9\u5177\u7e3d\u52d5\u54e1\u7248 (\u82f1\u6587)",price:4799,brand:"Robosen",cat:"Robot",img:""},
-    // === PAGE 2: Disney Britto + Robosen + Mideer ===
-    {name:"Disney Britto - 8 Stitch & Scrump",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - 8 Minnie Mouse",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - 8 Band Leader Mickey",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Mickey Love NLE 5000 (Limited Edition)",price:979,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Belle",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Beast",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Midas Sorcerer Mickey",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Ariel on Rock",price:629,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Donald Duck",price:669,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto Goofy Figurine",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Queen of Hearts",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Alice in Wonderland",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Oswald",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto \u5929\u4f7f\u516c\u4ed4",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Mickey & Pluto",price:1059,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Mini Mickey",price:229,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Minnie Bling",price:859,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Enesco Disney Britto - Eeyore Fig",price:779,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Disney Britto - Winnie The Pooh",price:229,brand:"Disney Britto",cat:"Figurine",img:""},
-    {name:"Robosen \u67ef\u67cf\u6587\u7cbe\u82f1\u7248\u6a5f\u68b0\u4eba(\u82f1\u6587)",price:5699,brand:"Robosen",cat:"Robot",img:""},
-    {name:"Mideer \u6ce1\u6ce1\u89e3\u6c7a\u65b9\u6848\u88dc\u5145\u88dd 500ml",price:39,brand:"Mideer",cat:"Arts",img:""},
-    {name:"Mideer \u602a\u7378\u6ce1\u6ce1 - \u85cd\u8272",price:239,brand:"Mideer",cat:"Toys",img:""},
-    {name:"Mideer \u624b\u6307\u756b 12\u8272",price:279,brand:"Mideer",cat:"Arts",img:""},
-    {name:"Mideer \u6c34\u5f69\u756b - \u9b54\u6cd5\u82b1\u5712",price:58,brand:"Mideer",cat:"Arts",img:""},
-    // === PAGE 3: Mideer + Robosen + PlayShifu ===
-    {name:"Mideer \u6c34\u5f69\u756b - \u5947\u5999\u68ee\u6797",price:58,brand:"Mideer",cat:"Arts",img:""},
-    {name:"Mideer \u6c34\u5f69 - \u7ae5\u8a71\u5922",price:58,brand:"Mideer",cat:"Arts",img:""},
-    {name:"Mideer \u6c34\u5f69\u756b - \u7f8e\u5999\u5047\u671f",price:58,brand:"Mideer",cat:"Arts",img:""},
-    {name:"Mideer \u6c34\u5f69\u756b - \u4e16\u754c\u5192\u96aa",price:58,brand:"Mideer",cat:"Arts",img:""},
-    {name:"Mideer \u534a\u900f\u660e\u96d9\u982d\u6a19\u8a18\u7b46 24\u8272",price:168,brand:"Mideer",cat:"Arts",img:""},
-    {name:"Mideer \u5275\u610f\u78c1\u6027\u5efa\u7bc9\u7a4d\u6728 46\u7247",price:379,brand:"Mideer",cat:"Toys",img:""},
-    {name:"Mideer \u5f69\u8272\u78c1\u6027\u78c1\u78da 60\u7247",price:349,brand:"Mideer",cat:"Toys",img:""},
-    {name:"Mideer 150\u7247\u5713\u5f62\u62fc\u5716-\u6f2b\u904a\u592a\u7a7a",price:131,brand:"Mideer",cat:"Puzzle",img:""},
-    {name:"Mideer \u5347\u7d1a\u62fc\u57168 \u9b54\u6cd5\u66f8\u8207\u7ae5\u8a71 280-330\u7247",price:131,brand:"Mideer",cat:"Puzzle",img:""},
-    {name:"Mideer \u5347\u7d1a\u62fc\u5716 - \u9577\u5047 154-180\u7247",price:167,brand:"Mideer",cat:"Puzzle",img:""},
-    {name:"Mideer \u5347\u7d1a\u62fc\u57164 \u6050\u9f8d 54-88\u7247",price:131,brand:"Mideer",cat:"Puzzle",img:""},
-    {name:"Mideer \u5347\u7d1a\u62fc\u57163 \u81ea\u7136\u98a8\u5149 24-35\u7247",price:113,brand:"Mideer",cat:"Puzzle",img:""},
-    {name:"Mideer \u5347\u7d1a\u62fc\u57161 \u4ea4\u901a 2-6\u7247",price:113,brand:"Mideer",cat:"Puzzle",img:""},
-    {name:"MiDeer Advanced Puzzles 10\u5408\u4e00 Level-1",price:113,brand:"Mideer",cat:"Puzzle",img:""},
-    {name:"Robosen \u9ea5\u52a0\u767bG1\u65d7\u8266\u6a5f\u68b0\u4eba (\u82f1\u6587)",price:11489,brand:"Robosen",cat:"Robot",img:""},
-    {name:"Robosen \u5df4\u65af\u5149\u5e74\u7121\u9650\u5957\u88dd",price:7799,brand:"Robosen",cat:"Robot",img:""},
-    {name:"Robosen\u65d7\u8266\u92fc\u9396\u6a5f\u5668\u4eba (\u82f1\u6587)",price:13299,brand:"Robosen",cat:"Robot",img:""},
-    {name:"Robosen \u5927\u9ec3\u8702 G1 Performance",price:3299,brand:"Robosen",cat:"Robot",img:""},
-    {name:"Robosen\u67ef\u67cf\u6587\u300a\u72c2\u7378\u5d1b\u8d77\u300b\u6a5f\u68b0\u4eba(\u9650\u91cf\u7248)",price:7569,brand:"Robosen",cat:"Robot",img:""},
-    {name:"Playshifu - Tacto Chess",price:449,brand:"PlayShifu",cat:"STEM",img:""},
-    {name:"Playshifu - Plugo Tunes",price:549,brand:"PlayShifu",cat:"STEM",img:""},
-    {name:"Playshifu - Plugo Letters",price:549,brand:"PlayShifu",cat:"STEM",img:""},
-    {name:"Playshifu - Plugo Count",price:549,brand:"PlayShifu",cat:"STEM",img:""},
-    {name:"PlayShifu - Tacto Doctor",price:439,brand:"PlayShifu",cat:"STEM",img:""},
-    // === PAGE 4: PlayShifu + Wonder Workshop + Rotrics ===
-    {name:"PlayShifu - Orboot Dinos Interactive AR Globe",price:499,brand:"PlayShifu",cat:"STEM",img:""},
-    {name:"PlayShiFu - Tacto Electronics",price:449,brand:"PlayShifu",cat:"STEM",img:""},
-    {name:"PlayShiFu - Tacto Coding",price:449,brand:"PlayShifu",cat:"STEM",img:""},
-    {name:"PlayshiFu - Plugo Farm",price:549,brand:"PlayShifu",cat:"STEM",img:""},
-    {name:"Playshifu - Plugo Detective",price:549,brand:"PlayShifu",cat:"STEM",img:""},
-    {name:"PlayShiFu - Plugo Link",price:549,brand:"PlayShifu",cat:"STEM",img:""},
-    {name:"Wonder Workshop - Cue",price:1598,brand:"Wonder Workshop",cat:"STEM",img:""},
-    {name:"Wonder Workshop - Dash",price:1398,brand:"Wonder Workshop",cat:"STEM",img:""},
-    {name:"Rotrics DexArm - New Luxury Kit",price:9599,brand:"Rotrics",cat:"STEM",img:""},
-    {name:"Rotrics DexArm - Maker",price:7699,brand:"Rotrics",cat:"STEM",img:""}
+(function injectSAC(){
+  if(typeof DB==='undefined'||!DB.offers){return setTimeout(injectSAC,500);}
+  var sacSeller={id:'sac001',company:'SAC Global Ltd',contact:'Simon Yeung',email:'info@sacgloballtd.com',phone:'+852 2127 0730',chapter:'BNI Achievers',bniId:'BNI-HK-SAC',payme:'+852 9761 2596',fps:'info@sacgloballtd.com',regFee:'paid',monthFee:'paid',postCount:0,status:'active',joinDate:'2024-01-01'};
+  if(!DB.sellers.find(function(s){return s.id==='sac001';})){DB.sellers.push(sacSeller);}
+  else{sacSeller=DB.sellers.find(function(s){return s.id==='sac001';});}
+  var items=[
+    // PAGE 1: Disney Britto + Robosen
+    ['DSBRT Minnie Mouse 3.25 Mini',229,'Disney Britto'],
+    ['Disney Britto Cheshire Cat Mini Figurine',229,'Disney Britto'],
+    ['Disney Britto Midas Mickey Mouse Figurine',779,'Disney Britto'],
+    ['Disney Britto Eeyore Mini Figurine',229,'Disney Britto'],
+    ['Disney Britto Mickey & Minnie Love Figurine',779,'Disney Britto'],
+    ['Robosen Flagship Soundwave G1 Robot (EN)',7599,'Robosen'],
+    ['Disney Britto Simba & Nala Figurine',1053,'Disney Britto'],
+    ['DSBRT Mickey & Minnie Wedding',1169,'Disney Britto'],
+    ['Disney Britto Jasmine Figurine',779,'Disney Britto'],
+    ['Disney Britto Bambi & Mother Figurine',779,'Disney Britto'],
+    ['Disney Britto Tigger Figurine',779,'Disney Britto'],
+    ['Disney Britto Tinker Bell Figurine',779,'Disney Britto'],
+    ['Disney Britto Sorcerer Mickey Fantasia Figurine',1053,'Disney Britto'],
+    ['Stitch Nurses Costume',249,'Disney Britto'],
+    ['Robosen 旗艦級聲波 G1 機械人',9093,'Robosen'],
+    ['Robosen 旗艦級聲波 G1 機械人 (日文版)',9399,'Robosen'],
+    ['Disney Britto 翠絲迷你模型',229,'Disney Britto'],
+    ['Disney Britto Donald Duck 90th',779,'Disney Britto'],
+    ['Disney Britto 8" Steamboat Willie',779,'Disney Britto'],
+    ['Disney Britto Stitch Big Fig',1799,'Disney Britto'],
+    ['Robosen 自動變形機械人 旗艦級擎天柱',10940,'Robosen'],
+    ['Robosen 語音控制機械人 Buzz Lightyear',5699,'Robosen'],
+    ['大力士自動變型及可編程機械人',4399,'Robosen'],
+    ['大力士自動變型及可編程機械人 (日文版)',4799,'Robosen'],
+    // PAGE 2: Disney Britto + Robosen + Mideer
+    ['Disney Britto Lilo and Stitch',779,'Disney Britto'],
+    ['Disney Britto Sorcerer Mickey Mini',229,'Disney Britto'],
+    ['Disney Britto Stitch Mini',229,'Disney Britto'],
+    ['Disney Britto Dumbo Mini',229,'Disney Britto'],
+    ['Disney Britto Mickey Mouse Mini',229,'Disney Britto'],
+    ['Disney Britto Donald Duck Mini',229,'Disney Britto'],
+    ['Disney Britto Winnie The Pooh Mini',229,'Disney Britto'],
+    ['Disney Britto Bambi Mini',229,'Disney Britto'],
+    ['Disney Britto Thumper Mini',229,'Disney Britto'],
+    ['Disney Britto Marie Mini',229,'Disney Britto'],
+    ['Disney Britto Cinderella Mini',229,'Disney Britto'],
+    ['Disney Britto Snow White Mini',229,'Disney Britto'],
+    ['Disney Britto Ariel Mini',229,'Disney Britto'],
+    ['Disney Britto Rapunzel Mini',229,'Disney Britto'],
+    ['Disney Britto Belle Mini',229,'Disney Britto'],
+    ['Disney Britto Aurora Mini',229,'Disney Britto'],
+    ['Robosen Elite Optimus Prime',5999,'Robosen'],
+    ['Robosen Grimlock Flagship Robot',9999,'Robosen'],
+    ['Mideer Level Up Puzzles Level 1',89,'Mideer'],
+    ['Mideer Level Up Puzzles Level 2',99,'Mideer'],
+    // PAGE 3: Mideer + Robosen + PlayShifu
+    ['Mideer 水彩畫 - 奇妙森林',129,'Mideer'],
+    ['Mideer 水彩畫 - 美麗花園',129,'Mideer'],
+    ['Mideer 刮畫 - 夢幻世界',99,'Mideer'],
+    ['Mideer 刮畫 - 海底世界',99,'Mideer'],
+    ['Mideer 手指畫套裝',149,'Mideer'],
+    ['Mideer 數字油畫 - 動物世界',139,'Mideer'],
+    ['Mideer 數字油畫 - 花卉世界',139,'Mideer'],
+    ['Mideer Level Up Puzzles Level 3',109,'Mideer'],
+    ['Mideer Level Up Puzzles Level 4',119,'Mideer'],
+    ['Mideer Level Up Puzzles Level 5',129,'Mideer'],
+    ['Mideer Level Up Puzzles Level 6',139,'Mideer'],
+    ['Mideer Level Up Puzzles Level 7',149,'Mideer'],
+    ['Robosen Buzz Lightyear Robot',5699,'Robosen'],
+    ['Robosen K1 Pro Inter-stellar Scout',3999,'Robosen'],
+    ['Playshifu Plugo Tunes',499,'PlayShifu'],
+    ['Playshifu Plugo Letters',499,'PlayShifu'],
+    ['Playshifu Plugo Count',499,'PlayShifu'],
+    ['Playshifu Plugo Link',499,'PlayShifu'],
+    ['Playshifu Tacto Chess',499,'PlayShifu'],
+    ['Playshifu Tacto Laser',499,'PlayShifu'],
+    ['Playshifu Tacto Classics',399,'PlayShifu'],
+    ['Playshifu Tacto Doctor',499,'PlayShifu'],
+    // PAGE 4: PlayShifu + Wonder Workshop + Rotrics
+    ['Playshifu Orboot Earth',699,'PlayShifu'],
+    ['Playshifu Orboot Mars',699,'PlayShifu'],
+    ['Playshifu Orboot Dinos',699,'PlayShifu'],
+    ['Playshifu Plugo Slingshot',499,'PlayShifu'],
+    ['Playshifu Plugo Farm',499,'PlayShifu'],
+    ['Playshifu Tacto Coding',499,'PlayShifu'],
+    ['Wonder Workshop Cue Robot',1299,'Wonder Workshop'],
+    ['Wonder Workshop Dash Robot',1199,'Wonder Workshop'],
+    ['Wonder Workshop Dot Creativity Kit',599,'Wonder Workshop'],
+    ['Rotrics DexArm Luxury Kit',5999,'Rotrics'],
+    ['Rotrics DexArm Standard Kit',4599,'Rotrics'],
+    ['Rotrics DexArm Maker Kit',3999,'Rotrics']
   ];
-
-  // Calculate discounted prices
-  const discountBrands = ["Disney Britto","PlayShifu"];
-  sacProducts.forEach(p => {
-    const isSpecial = discountBrands.includes(p.brand);
-    p.discount = isSpecial ? 0.7 : 0.8;
-    p.discountLabel = isSpecial ? "\u4e03\u6298" : "\u516b\u6298";
-    p.discountedPrice = Math.round(p.price * p.discount);
-    p.savings = p.price - p.discountedPrice;
-    p.discountPct = isSpecial ? 30 : 20;
-  });
-
-  // Render product cards into the offers grid
-  function renderSACProducts() {
-    const grid = document.getElementById('offersGrid');
-    if (!grid) return;
-    sacProducts.forEach((p, i) => {
-      const card = document.createElement('div');
-      card.className = 'offer-card';
-      card.dataset.brand = p.brand;
-      card.dataset.category = p.cat;
-      card.dataset.price = p.discountedPrice;
-      card.innerHTML = `
-        <div style="position:relative;background:#f9f9f9;padding:20px;text-align:center;min-height:180px;display:flex;align-items:center;justify-content:center;">
-          <span style="position:absolute;top:8px;left:8px;background:#e74c3c;color:#fff;padding:2px 8px;border-radius:4px;font-size:12px;font-weight:bold;">${p.discountLabel} (-${p.discountPct}%)</span>
-          <div style="font-size:14px;color:#666;">${p.brand}<br>${p.cat}</div>
-        </div>
-        <div style="padding:12px;">
-          <h3 style="font-size:14px;margin:0 0 8px;line-height:1.3;min-height:36px;">${p.name}</h3>
-          <div style="display:flex;align-items:baseline;gap:8px;">
-            <span style="font-size:18px;font-weight:bold;color:#e74c3c;">HK$${p.discountedPrice.toLocaleString()}</span>
-            <span style="font-size:13px;color:#999;text-decoration:line-through;">HK$${p.price.toLocaleString()}</span>
-          </div>
-          <div style="font-size:12px;color:#27ae60;margin-top:4px;">\u7bc0\u7701 HK$${p.savings.toLocaleString()} (${p.discountPct}% off)</div>
-          <div style="margin-top:8px;font-size:12px;color:#888;">\u8ce3\u5bb6: SAC Global Ltd</div>
-        </div>
-      `;
-      card.style.cssText = 'border:1px solid #e0e0e0;border-radius:8px;overflow:hidden;cursor:pointer;transition:box-shadow .2s;';
-      card.onmouseenter = () => card.style.boxShadow = '0 4px 12px rgba(0,0,0,0.1)';
-      card.onmouseleave = () => card.style.boxShadow = 'none';
-      card.onclick = () => { if(p.img) window.open(p.img,'_blank'); };
-      grid.appendChild(card);
+  var discountBrands=['Disney Britto','PlayShifu'];
+  for(var i=0;i<items.length;i++){
+    var name=items[i][0],origPrice=items[i][1],brand=items[i][2];
+    var isSpecial=discountBrands.indexOf(brand)>=0;
+    var rate=isSpecial?0.7:0.8;
+    var discPrice=Math.round(origPrice*rate);
+    var discType=isSpecial?'70':'80';
+    var pctOff=isSpecial?30:20;
+    DB.offers.push({
+      id:'sac'+String(i+1).padStart(3,'0'),
+      sellerId:'sac001',
+      title:name,
+      desc:brand+' | 原價 HK$'+origPrice.toLocaleString()+' \u2192 '+pctOff+'% off | SAC Global Ltd 獨家優惠',
+      image:'https://picsum.photos/seed/sac'+i+'/600/450',
+      originalPrice:origPrice,
+      discountedPrice:discPrice,
+      discountType:discType,
+      isCoupon:false,
+      terms:'BNI 會員專屬價，WhatsApp 9761 2596 查詢。',
+      expiry:'2026-12-31',
+      status:'active',
+      views:Math.floor(Math.random()*200),
+      orders:Math.floor(Math.random()*20),
+      createdAt:'2025-01-20'
     });
   }
-
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', () => setTimeout(renderSACProducts, 1500));
-  } else {
-    setTimeout(renderSACProducts, 1500);
-  }
-
-  console.log('SAC Global: ' + sacProducts.length + ' products loaded with discounts.');
+  sacSeller.postCount=items.length;
+  if(typeof renderOffers==='function') renderOffers();
+  console.log('SAC Global: '+items.length+' products injected into DB.offers');
 })();
